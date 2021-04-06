49c84b8a0ade33f9ceff58c8f6677f49c7521d74
DAXCREZ <ldiegobayona@unicesar.edu.co>
2021-04-06 16:02:43 -0500

    Menu Principal Se termino la estructura con casi el total del diseño, falta animaciones y duraciones de estas.

--- src/componentes/menu-header/Menu-header.js
+++ src/componentes/menu-header/Menu-header.js
@@ -9,23 +9,40 @@ class MenuHeader extends Component {
                         <img className="logo" src={clinica} alt=""/>
                     </i>
                     <ul className="menu__contenedor">
-                        <li className="menu__contenedor__opciones"><a href="">Inicio</a></li>
-                        <li className="menu__contenedor__opciones">
-                            <a href="">
-                                COFCA
-                                <i className="fas fa-angle-down"></i>
-                            </a>
+                        <li className="menu__contenedor__opciones"><a className="a-opciones" href="">Inicio</a></li>
+                        <li className="menu__contenedor__opciones"><a className="a-opciones" href="">COFCA<i className="fas fa-angle-down"></i></a>
+                            <ul className="submenu">
+                                <li className="sublista"><a className="sub-a" href="">Quienes somos</a></li>
+                                <li className="sublista"><a className="sub-a" href="">Historia</a></li>
+                                <li className="sublista"><a className="sub-a" href="">Nuestras Citas</a></li>
+                                <li className="sublista"><a className="sub-a" href="">Premios y Certificaciones</a></li>
+                                <li className="sublista"><a className="sub-a" href="">Sedes</a></li>
+                            </ul>
                         </li>
                         <li className="menu__contenedor__opciones">
-                            <a href="">
+                            <a className="a-opciones" href="">
                                 Pacientes
                                 <i className="fas fa-angle-down"></i>
                             </a>
+                            <ul className="submenu">
+                                <li className="sublista"><a className="sub-a" href="">Especialidades</a></li>
+                                <li className="sublista"><a className="sub-a" href="">Equipo medico</a></li>
+                            </ul>
                         </li>
-                        <li className="menu__contenedor__opciones"><a href="">Educacion</a></li>
-                        <li className="menu__contenedor__opciones"><a href="">Investigacion</a></li>
-                        <li className="menu__contenedor__opciones"><a href="">FOCA</a></li>
-                        <li className="menu__contenedor__opciones"><a href="">Contacto</a></li>
+                        <li className="menu__contenedor__opciones"><a className="a-opciones" href="">Educacion</a></li>
+                        <li className="menu__contenedor__opciones">
+                            <a className="a-opciones" href="">
+                                Investigacion
+                                <i className="fas fa-angle-down"></i>
+                            </a>
+                            <ul className="submenu">
+                                <li className="sublista"><a className="sub-a" href="">¿Que es CIRVO?</a></li>
+                                <li className="sublista"><a className="sub-a" href="">Equipo de investigacion</a></li>
+                                <li className="sublista"><a className="sub-a" href="">Comite de etica</a></li>
+                            </ul>
+                        </li>
+                        <li className="menu__contenedor__opciones"><a className="a-opciones" href="">FOCA</a></li>
+                        <li className="menu__contenedor__opciones"><a className="a-opciones" href="">Contacto</a></li>
                     </ul>
                 </nav>
         );
