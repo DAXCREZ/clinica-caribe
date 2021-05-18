962b7d74c2d57ac9ae73e6d83fd752ba13b6eb93
DAXCREZ <ldiegobayona@unicesar.edu.co>
2021-05-18 13:12:10 -0500

    Menu Citas Se termino el menu citas, se procede a realizar el banner o imagen y todo el main.

--- src/componentes/menu-citas/Menu-citas.js
+++ src/componentes/menu-citas/Menu-citas.js
@@ -9,6 +9,28 @@ class MenuCitas extends Component {
                             <a className="menucitas__opciones--a" href="">Proteccion de datos
                                 <i className="fas fa-chevron-down"></i>
                             </a>
+                            <ul className="menucitas__submenu">
+                                <li className="menucitas__submenu__opciones">
+                                    <a href="" className="menucitas__submenu__opciones__link"><i className="fas fa-user"></i>
+                                    Aviso de Privacidad</a>
+                                </li>
+
+                                <li className="menucitas__submenu__opciones">
+                                    <a href="" className="menucitas__submenu__opciones__link"><i className="fas fa-user"></i>
+                                        Politica de Proteccion de Datos Personales</a>
+                                </li>
+
+                                <li className="menucitas__submenu__opciones">
+                                    <a href="" className="menucitas__submenu__opciones__link"><i className="fas fa-user"></i>
+                                        Politica de Cookies</a>
+                                </li>
+
+                                <li className="menucitas__submenu__opciones">
+                                    <a href="" className="menucitas__submenu__opciones__link"><i className="fas fa-user"></i>
+                                        Politica de Propiedad Intelectual e Industrial</a>
+                                </li>
+
+                            </ul>
                         </li>
                         <li className="menucitas__opciones">
                             <a className="menucitas__opciones--a" href="">Pide tu cita
